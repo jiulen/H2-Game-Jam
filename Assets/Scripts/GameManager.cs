1cfe12d17232cec59085f00df7c283b7f755f4ef
JONATHAN ONG <211788R@mymail.nyp.edu.sg>
2023-10-10 09:56:56 +0800

    maingame + game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public enum StateType
 {
-    open,           // game opened, load main menu
-    end,            // load main menu
-    levelChange,    // load next level
-    respawn,        // re-load level again when player dies
-    credits         // show credits
+    menu,
+    gameplay,
+    death,
+    respawn,  
+    credits   
 }
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance = null;
+    public static GameManager instance;
     public StateType state;
     public bool resetPLayer;
     public int highscore;
+
+    public GameObject GameUI;
+    public GameObject MenuUI;
+
+    private Touch touch;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +32,68 @@ public class GameManager : MonoBehaviour
         }
         else
             Destroy(this);
+
+        SetGameState(StateType.menu);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+        }
+
+        // for mobile
+        if (Input.touchCount > 0 && touch.phase == TouchPhase.Ended && state == StateType.menu)
+        {
+            if (Mathf.Abs(touch.deltaPosition.y) == Mathf.Abs(touch.deltaPosition.x))
+            {
+                SetGameState(StateType.gameplay);
+            }
+        }
+
+        //for pc
+        if (Input.GetKeyDown(KeyCode.Mouse0) && state == StateType.menu)
+        {
+            SetGameState(StateType.gameplay);
+        }
+    }
+
+    void SetGameState(StateType newState)
+    {
+        state = newState;
+
+        switch(state)
+        {
+            case StateType.menu:
+                {
+                    MenuUI.SetActive(true);
+                    GameUI.SetActive(false);
+                    CameraScript.instance.moving = false;
+                    break;
+                }
+
+            case StateType.gameplay:
+                {
+                    GameUI.SetActive(true);
+                    MenuUI.SetActive(false);
+                    CameraScript.instance.moving = true;
+                    break;
+                }
+
+            case StateType.death:
+                {
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        instance = null;
     }
 }
