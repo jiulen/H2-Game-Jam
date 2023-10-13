34815ce420885be9025fd1fd748bc9ac9fa6635e
jiulen <wongjiulen.coding@gmail.com>
2023-10-13 17:47:01 +0800

    gacha animation done + new water + more stuff

---
+++ Assets/Scripts/GachaManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaManager : MonoBehaviour
+{
+    public GameObject gachaObj;
+    public Animator gachaAnimator;
+    AnimatorStateInfo animatorStateInfo;
+    public float animationTime;
+
+    public bool waiting = true;
+    public float waitTime;
+    public float waitTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrepareAnim(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waiting)
+        {
+            waitTimer += Time.deltaTime;
+
+            if (waitTimer > waitTime)
+            {
+                gachaAnimator.speed = 1;
+                waiting = false;
+            }
+        }
+        else
+        {
+            animatorStateInfo = gachaAnimator.GetCurrentAnimatorStateInfo(0);
+            animationTime = animatorStateInfo.normalizedTime;
+
+            if (animationTime > 1)
+            {
+                GameManager.instance.OpenGachaResult(true);
+                GameManager.instance.OpenGacha(false);
+
+                PrepareAnim(false);
+            }
+        }
+
+
+    }
+
+    public void PrepareAnim(bool prepare)
+    {
+        gachaObj.SetActive(prepare);
+
+        if (prepare)
+        {
+            waiting = true;
+            gachaAnimator.speed = 0;
+            gachaAnimator.Play("gachafishanim", -1, 0);
+
+            Debug.Log("b");
+        }
+    }
+}
